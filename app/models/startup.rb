1e3dba8994aba302ef3fc86aa6cfd78f4174d716
fresh <drewsdhoffer@gmail.com>
2025-06-07 16:56:12 -0400

    test commit

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,5 +1,5 @@
 class Startup < ApplicationRecord
   acts_as_taggable_on :tags
   validates :name, presence: true
-  validates :description, presence: true, length: { minimum: 50, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 25, maximum: 500 }
 end
